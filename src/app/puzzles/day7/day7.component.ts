84aef4d223cfb47e9331179e89f254d9deabd39b
N.G <nathan.j.grealish@gmail.com>
2020-12-07 12:17:31 -0500

    Fixing day 7

--- src/app/puzzles/day7/day7.component.ts
+++ src/app/puzzles/day7/day7.component.ts
@@ -26,28 +26,10 @@ export class Day7Component implements OnInit {
   }
 
   cleanRules() {
-    // const newRules = [];
     this.rules.forEach(rule => {
       rule.forEach((bag, i) => {
         rule[i] = bag.substring(0, bag.lastIndexOf(' '));
       });
-      // let newRule = {};
-      // console.log(rule);
-      // newRule = {
-      //   [rule[0]]: []
-      // };
-      //
-      // rule.forEach((bag, i) => {
-      //   if (i > 0) {
-      //     const count = bag.split(' ')[0];
-      //     const type = bag.substr(bag.indexOf(' ') + 1);
-      //     const newBag = {[type] : count};
-      //     console.log(newBag);
-      //     [rule[0]].push(newBag);
-      //   }
-      // });
-      //
-      // console.log(newRule);
     });
   }
 
@@ -58,14 +40,10 @@ export class Day7Component implements OnInit {
   }
 
   puzzle2() {
-    this.p2Containers = [];
-    this.p2TotalBags = 1;
-    this.p2Bags = {
-      'shiny gold': 1,
-    };
+    this.p2TotalBags = 0;
     this.findBagsUp('shiny gold', 1, 1);
-    this.countBagsObject();
     console.log('total bags', this.p2TotalBags);
+    this.p2num = this.p2TotalBags;
   }
 
   findBagsDown(bagType) {
@@ -84,48 +62,22 @@ export class Day7Component implements OnInit {
   }
 
   findBagsUp(bagType, multiplier, total) {
-    console.log(' ');
-    console.log('Find bags in the container of:', bagType);
     this.rules.forEach(rule => {
       rule.forEach((r, i) => {
         if (r.includes(bagType) && i === 0) {
           // This rule is the bagType bag as a container
-          console.log(rule);
-          rule.forEach((bag, j) => {
-            if (j > 0) {
-              // This is each bag that's not a container
-              const type = bag.substr(bag.indexOf(' ') + 1);
+          const contents = rule.map((x) => x); // clone the array to keep it sterile
+          contents.shift(); // remove the first item
+          contents.forEach((bag, j) => {
+            const type = bag.substr(bag.indexOf(' ') + 1);
+            if (type !== 'other') {
               const count = parseInt(bag.split(' ')[0], 0) * multiplier;
-
-              if (!Number.isNaN(count)) { // If there is a number (not other)
-                console.log(multiplier, bagType, 'contains', count, type);
-                // console.log(total + count, 'total bags so far');
-
-                if (type in this.p2Bags) {
-                  this.p2Bags[type] = count; // TODO Probably have to change this line.
-                } else {
-                  this.p2Bags[type] = count;
-                }
-
-              } else {
-                // console.log(total + multiplier, 'END total bags');
-                this.p2TotalBags += total;
-              }
+              this.p2TotalBags += count;
               this.findBagsUp(type, count, total + count);
             }
           });
-
         }
       });
     });
   }
-
-  countBagsObject() {
-    for (let count of Object.keys(this.p2Bags)) {
-      count = this.p2Bags[count];
-      console.log(count);
-      this.p2TotalBags += count;
-    }
-  }
-
 }
